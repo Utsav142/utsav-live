b84f40ef30cbdf53cad4361a0e84a39edb0ad2f3
Utsav142 <87812874+Utsav142@users.noreply.github.com>
2022-06-08 01:57:25 +0530

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s4u#vg*xqqqd2(6g6^-gq2q(j=m(wma)1r5k)h2)3#$7gd5(@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['utsav1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
